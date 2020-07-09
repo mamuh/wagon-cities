efe8bbeaef416a0decac2d0b0ac2dafacae60e58
Mariana Caramori <musardo@mail.com>
2020-07-09 20:12:25 -0300

    map undefined

--- src/containers/city_list.jsx
+++ src/containers/city_list.jsx
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 
+// import {bindActionCreators} from 'redux';
+// import {connect} from 'react-redux';
+// import {setCities} from '../actions';
+
 import City from '../containers/city.jsx';
 
 class CityList extends Component {
   constructor(props) {
     super(props);
   }
+
+  // componentWillMount() {
+  //   this.props.setCities();
+  // }
+
+  // function mapDispatchToProps(dispatch) {
+  //   return bindActionCreators(
+  //   {
+  //     setCities: setCities
+  //   }, dispatch
+  //   );
+  // }
 
   render() {
     const { cities } = this.props;
